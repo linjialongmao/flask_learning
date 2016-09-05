c5cb20d36d659209d729dade41fd3d3c86a35a0e
linjialongmao <jialeikang@foxmail.com>
2016-09-05 19:23:38 +0800

    wtf

--- flask0830.py
+++ flask0830.py
@@ -1,25 +1,39 @@
-# coding = utf-8
+﻿# coding = utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard '
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 manager = Manager(app)
+CODEC = 'utf-8'
+
+class NameForm(Form):
+    name = StringField(u'请输入你的名字?')
+    submit = SubmitField(u'提交')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name != None and old_name != form.name.data:
+            flash(u'输入名字不一致，请重新输入')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', name=session.get('name'), form=form)
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name)
+    return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
